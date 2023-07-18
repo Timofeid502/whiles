bd1b5fd9f49d48fe9c041669b7c3f106585154fe
Timofeid502 <139252091+Timofeid502@users.noreply.github.com>
2023-07-18 09:29:17 -0600

    Add files via upload

---
+++ mush.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () { int numero;
+cout<<"ingrese un numero";
+cin>>numero;
+while (numero<=100)
+{cout<<"ingrese un numero";
+cin>>numero;}
+return 0;
+}
